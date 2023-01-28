995e9d0beafd620501b55cbca0f5396b1bc02522
KarpAnt <antonkarp95@mail.ru>
2023-01-28 14:41:18 +0300

    Changed the splash screen installation method to the SplashScreenApi

--- app/src/main/java/com/example/nw/MainActivity.kt
+++ app/src/main/java/com/example/nw/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.nw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Nw)
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
